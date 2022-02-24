ab5e73541232ba70db935330390355cb7662ae63
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-24 23:25:18 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -14,3 +14,38 @@ TEST_CASE("infers the breach to higher limit crossed") {
 TEST_CASE("infers the breach is normal") {
   REQUIRE(inferBreach(40, 5, 40) == NORMAL);
 }
+
+TEST_CASE("Infers the high breach according to passive cooling limits and sending to e-mail") {
+  BatteryCharacter batteryChar = {PASSIVE_COOLING, "PASSIVE_COOLING_HIGH_BREACH"};
+  REQUIRE(classifyBreachAndAlert(TO_EMAIL, batteryChar, 36) == ALERT_SUCCESS);
+}
+
+TEST_CASE("infers the low breach according to passive cooling limits and sending to controller") {
+  BatteryCharacter batteryChar = {PASSIVE_COOLING, "PASSIVE_COOLING_LOW_BREACH"};
+  REQUIRE(classifyBreachAndAlert(TO_CONTROLLER, batteryChar, -2) == ALERT_SUCCESS);
+}
+
+TEST_CASE("infers the no breach according to high active cooling limits and sending to Console") {
+  BatteryCharacter batteryChar = {HI_ACTIVE_COOLING, "NORMAL_HI_ACTIVE_COOLING"};
+  REQUIRE(classifyBreachAndAlert(TO_EMAIL, batteryChar, 45) == ALERT_SUCCESS);
+}
+
+TEST_CASE("infers the high breach according to high active cooling limits and sending to e-mail") {
+  BatteryCharacter batteryChar = {HI_ACTIVE_COOLING, "HI_ACTIVE_COOLING_HIGH_BREACH"};
+  REQUIRE(classifyBreachAndAlert(TO_EMAIL, batteryChar, 46) == ALERT_SUCCESS);
+}
+
+TEST_CASE("infers the low breach according to high active cooling limits and sending to controller") {
+  BatteryCharacter batteryChar = {HI_ACTIVE_COOLING, "HI_ACTIVE_COOLING_LOW_BREACH"};
+  REQUIRE(classifyBreachAndAlert(TO_EMAIL, batteryChar, -2) == ALERT_SUCCESS);
+}
+
+TEST_CASE("infers the high breach according to med active cooling limits and sending to e-mail") {
+  BatteryCharacter batteryChar = {MED_ACTIVE_COOLING, "MED_ACTIVE_COOLING_HIGH_BREACH"};
+  REQUIRE(classifyBreachAndAlert(TO_EMAIL, batteryChar, 41) == ALERT_SUCCESS);
+}
+
+TEST_CASE("infers the low breach according to med active cooling limits and sending to controller") {
+  BatteryCharacter batteryChar = {MED_ACTIVE_COOLING, "MED_ACTIVE_COOLING_LOW_BREACH"};
+  REQUIRE(classifyBreachAndAlert(TO_CONTROLLER, batteryChar, -2) == ALERT_SUCCESS);
+}

8c4b1b0418ccfbd7142cbadd3e0af5d4978cd134
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-24 21:51:34 +0530

    Add cases for normal and higher limits

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,14 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to lower limit crossed") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+}
+
+TEST_CASE("infers the breach to higher limit crossed") {
+  REQUIRE(inferBreach(42, 10, 40) == TOO_HIGH);
+}
+
+TEST_CASE("infers the breach is normal") {
+  REQUIRE(inferBreach(40, 5, 40) == NORMAL);
 }
